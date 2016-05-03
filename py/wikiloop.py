4ad00197e5960c8ad8c06fba2b4ca65004a54bea
WorkOfArtiz <arghhhhhthur@gmail.com>
2016-05-03 22:09:07 +0200

    updated some stuffs

--- py/wikiloop.py
+++ py/wikiloop.py
@@ -1,85 +1,91 @@
 import wikipedia
+import re
+import time
+import os
+import sys
+from collections import defaultdict
+from codecs import open
 from bs4 import BeautifulSoup
 
+# data structure for keeping track of where we went
+visited = defaultdict(bool)
 
+# init caching mechanism
+if not os.path.exists('data'):
+    os.makedirs('data')
+
+illegal_filechars = re.compile(r'[\s#%&{}()\\<>*@:"\'$!]')
 def get_html(title):
-    return wikipedia.page(title).html()
+    local_path = "data/%s.html" % illegal_filechars.sub('', title)
 
-def hasAttribute(obj,attr):
-    try:
-        whatevs = obj[attr]
-        return True
-    except:
-        return False
+    # if cached
+    if os.path.exists(local_path):
+        with open(local_path) as f:
+            return f.read()
+    else:
+        try:
+            html = wikipedia.page(title).html()
+        except wikipedia.exceptions.DisambiguationError as e:
+            html = wikipedia.page(e.options[0]).html()
 
-def isOk(a):
-    return not isLangLink(a) and hasAttribute(a,'title') and 'wikt:' not in a['title'] and '#cite_note' not in a['href'] and 'Help:' not in a['href'] and '.ogg' not in a['href'] and 'wikipedia/commons' not in a['href'] and 'Wikipedia:' not in a['href']
+        with open(local_path, 'w+', 'utf-8') as f:
+            f.write(html)
+    return html
 
-def isInfoBox(ele):
-    try:
-        res = 'vcard' in ele['class']
-    except:
-        res = False
+# wiki pages have info boxes, spam etc.
+# it sucks
+def remove_shit(soup):
+    rm_classes = ["infobox", "toc", 'IPA', 'internal', 'extiw']
+    for notebox in soup.find_all(True, {"class":rm_classes}):
+        notebox.extract()
 
-    return res
+def filter_anchors(anchors):
+    results = []
+    for jump in anchors:
+        try:
+            title = jump['title']
+            if any(jump['href'].startswith(junk) for junk in ['#', 'File:']) \
+                or not title \
+                or title.startswith("Special:") \
+                or title.endswith("(page does not exist)"):
+                continue
+        except:
+            continue
 
-def isLangLink(a):
-    return "_language" in a['href']
+        if title and not visited[title]:
+            results.append(title.strip())
+    return results
 
 def get_title_of_first_link_of_page(page_title):
+    visited[page_title] = True
+
     html = get_html(page_title)
-    soup = BeautifulSoup(html,'html.parser')
+    soup = BeautifulSoup(html,'lxml')
+    remove_shit(soup)
+    titles = filter_anchors(soup.find_all('a'))
+    if titles:
+        return titles
+    raise Exception("Couldn't find a valid link on this useless article")
 
-    yes = False
-    for table in soup.findAll('table'):
-        if isInfoBox(table):
-            yes = True
-        #    print(table)
-            break
+if __name__ == '__main__':
+    titles = ['Deterministic context-free language']
 
-  #  print(yes)
+    while titles:
+        title = titles.pop()
+        try:
+            print(title)
+            titles = get_title_of_first_link_of_page(title) + titles
+            if len(titles) > 100:
+                titles = titles[:100]
+        except KeyboardInterrupt as e:
+            print("You wanted me stopped, father :(\ny u do tis 2 me")
+            exit(0)
+        except Exception as e:
+            print(e.message)
 
-    if yes:
-        candidates = soup.find_all()
-        res = None
-        passedInfoBox = False
-        count = 0
-        while res==None or not passedInfoBox and count<len(candidates):
+        print("stats:")
+        print("size visited map: %d" % sys.getsizeof(visited))
+        print("size titles list: %d" % sys.getsizeof(titles))
+        time.sleep(1)
 
-            if not passedInfoBox:
-                if isInfoBox(candidates[count]):
-                    passedInfoBox=True
-                count=count+1
-                continue
-
-            stri = str(candidates[count])
-            if stri[0]=='<' and stri[1]=='p' and stri[2]=='>':
-            #    print(candidates[count])
-                for a in candidates[count].find_all('a'):
-                    if isOk(a):
-                        #print("selected ",a['href'])
-                        res = a['title']
-                        break
-
-            count=count+1
-        return res
-    else:
-
-        candidates = soup.find_all('p')
-        res = None
-        count = 0
-        while res==None and count < len(candidates):
-            summary = candidates[count]
-            print(summary[0:100])
-            count=count+1
-            for a in summary.find_all('a'):
-                if isOk(a):
-              #      print("selected ",a['href'])
-                    res = a['title']
-                    break
-        return res
-
-title = "Bob Dylan"
-while True:
-    print(title)
-    title = get_title_of_first_link_of_page(title)
+    print("No titles left to follow :/")
